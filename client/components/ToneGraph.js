12443ed8d204ba2d11415cbc4ee530c5b5dd5483
jt wang <jtjtjtjtw@gmail.com>
2018-01-31 16:25:52 -0600

    works jankily

--- client/components/ToneGraph.js
+++ client/components/ToneGraph.js
@@ -127,7 +127,50 @@ class ToneGraph extends React.Component {
   }
 
   componentDidMount(){
-    this.props.getEntryData(this.props.user.id, this.prepData)
+    if (!this.props.allEntries.length > 0){
+      this.props.getEntryData(this.props.user.id, this.selectEntries, this.calculateDataFunc, this.prepData)
+    }
+  }
+
+  selectEntries = (entries, range ) => {
+    let nowDate = new Date();
+    nowDate.setHours(0, 0, 0, 0)
+    let beginRangeDate = new Date();
+    if (range === 'last7Days'){
+      beginRangeDate.setDate(nowDate.getDate() - 7)
+      beginRangeDate.setHours(0, 0, 0, 0 )
+    }
+    if (!range || range === 'last30Days'){
+      beginRangeDate.setDate(nowDate.getDate() - 30)
+      beginRangeDate.setHours(0, 0, 0, 0)
+    }
+    let filteredEntries = entries.filter((entry) => {
+      return new Date(entry.savedAt) >= beginRangeDate && new Date(entry.savedAt) <= nowDate
+    })
+    return filteredEntries
+  }
+
+  calculateDataFunc = (filtered) => {
+    console.log('filtered entries', filtered)
+    let dataObj = {}
+    let count = {}
+    filtered.forEach((entry) => {
+      let savedAt = entry.savedAt.slice(0, entry.savedAt.indexOf('T'))
+      if (!dataObj[savedAt]){
+        dataObj[savedAt] = entry.tones
+        count[savedAt] = 1
+      } else {
+        count[savedAt]++
+        entry.tones.forEach((entryTone) => {
+          let thing = dataObj[savedAt].find((tone) => {
+            return tone.tone_id === entryTone.tone_id
+          })
+          thing.score = (thing.score + entryTone.score) / count[savedAt]
+        })
+      }
+    })
+    console.log('dataObj', dataObj)
+    return dataObj
   }
 
   prepData = (entries) => {
@@ -141,18 +184,19 @@ class ToneGraph extends React.Component {
       confident: {"color": "hsl(163, 70%, 50%)"},
       tentative: {"color": "hsl(90, 70%, 50%)"}
     }
+
     for (let tone in tones) {
       if (tones.hasOwnProperty(tone)){
         let entryData = []
-        entries.forEach((entry) => {
-          let value = entry.tones.find((toneIn) => {
-            return toneIn.tone_id === tone
+        for (let day in entries){
+          let value = entries[day].find((obj) => {
+            return obj.tone_id === tone
           })
           entryData.push({
-            "x": `${new Date(entry.savedAt)}`,
+            "x": day,
             "y": value ? value.score : 0,
           })
-        })
+        }
         graphData.push({
           "id": tone,
           "color": tones[tone].color,
@@ -172,21 +216,21 @@ class ToneGraph extends React.Component {
         {this.state.toneGraphData && this.state.toneGraphData.length > 0 &&
           <Line
             data={this.state.toneGraphData}
-            width={1000}
+            width={800}
             height={500}
             margin={{
                 "top": 35,
-                "right": 110,
+                "right": 100,
                 "bottom": 50,
-                "left": 60
+                "left": 40
             }}
             minY="auto"
             axisBottom={{
                 "orient": "bottom",
-                "tickSize": 5,
-                "tickPadding": 5,
+                "tickSize": 1,
+                "tickPadding": 3,
                 "tickRotation": 0,
-                "legend": "country code",
+                "legend": "entries",
                 "legendOffset": 36,
                 "legendPosition": "center"
             }}
@@ -200,7 +244,7 @@ class ToneGraph extends React.Component {
                 "legendPosition": "center"
             }}
             lineWidth={4}
-            dotSize={10}
+            dotSize={8}
             dotColor="inherit:darker(0.3)"
             dotBorderWidth={2}
             dotBorderColor="#ffffff"
@@ -238,10 +282,16 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    getEntryData: (userId, dataFunc) => {
+    getEntryData: (userId, entryFilterFunc, calculateFunc, prepFunc) => {
       return dispatch(getEntriesDb(userId))
       .then((actionType) => {
-        dataFunc(actionType.entries)
+        return entryFilterFunc(actionType.entries)
+      })
+      .then((filteredEntries) => {
+        return calculateFunc(filteredEntries)
+      })
+      .then((preppedEntries) => {
+        return prepFunc(preppedEntries)
       })
     },
   }
